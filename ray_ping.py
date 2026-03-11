27727541efb77f1135aa3b8b3b06cb150edc927a
bricen <code@bricen.net>
2026-03-11 00:26:05 -0500

    chore: modified implementation to allow configurable number of nodes

--- ray_ping.py
+++ ray_ping.py
@@ -4,29 +4,37 @@ import socket
 import ray
 
 
+EXPECTED_RAY_NODES = int(os.environ.get("EXPECTED_RAY_NODES", "2"))
+
 ray.init(address=os.environ["RAY_ADDRESS"], ignore_reinit_error=True)
 
-# Wait until both nodes are visible
+# Wait until all expected nodes are visible
 while True:
     nodes = [n for n in ray.nodes() if n["Alive"]]
-    if len(nodes) >= 2:
+    if len(nodes) >= EXPECTED_RAY_NODES:
         break
     time.sleep(2)
 
-print("\nRay cluster nodes:")
-for n in nodes:
-    name = n.get("NodeName")
-    ip = n.get("NodeManagerAddress")
-    print(f"{name}  ({ip})")
+print(f"\nRay cluster nodes ({len(nodes)}/{EXPECTED_RAY_NODES} visible):")
+for i, n in enumerate(nodes, 1):
+    print(
+        f"  node {i}: "
+        f"ip={n.get('NodeManagerAddress')} "
+        f"hostname={n.get('NodeName')} "
+        f"alive={n.get('Alive')}"
+    )
+
 
 @ray.remote
 def ping():
     return {
         "hostname": socket.gethostname(),
-        "pid": os.getpid()
+        "ip": socket.gethostbyname(socket.gethostname()),
+        "pid": os.getpid(),
     }
 
-results = ray.get([ping.remote() for _ in nodes])
+
+results = ray.get([ping.remote() for _ in range(len(nodes))])
 
 print("\nPing results:")
 for r in results:
